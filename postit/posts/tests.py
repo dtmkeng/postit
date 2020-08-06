421f19129a7ba943f1c03db7d4f767e7172750b4
dtmkeng <phumai@odds.team>
2020-08-06 23:39:08 +0700

    create test models

--- postit/posts/tests.py
+++ postit/posts/tests.py
@@ -1,7 +1,25 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
 class PostTest(TestCase):
-    #Give
-    #When
-    #Then
+    def test_is_shoud_instance_of_post(self): 
+        self.assertEqual(isinstance(Post.objects.create(), Post), True)
+        
+    def test_is_should_create_post_model(self):
+        #Give
+        caption = 'new normal'
+        post_by = 'pm. jan'
+        location = 'thai'
+        #When
+        
+        result = Post.objects.create(
+            caption = caption,
+            post_by = post_by,
+            location = location,
+        )
+
+        #Then
+        self.assertEqual(result.caption, caption)
+        self.assertEqual(result.post_by, post_by)
+        self.assertEqual(result.location, location)
