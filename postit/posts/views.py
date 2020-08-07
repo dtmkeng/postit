7af20e91f5b0789b2b9df93fcb7df6fd67132439
dtmkeng <phumai@odds.team>
2020-08-07 23:32:18 +0700

    render view with html template

--- postit/posts/views.py
+++ postit/posts/views.py
@@ -1,9 +1,10 @@
 from django.views import View
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 class PostView(View):
     def get(self, request):
-        html = '<div>Hello</div>'
-        return HttpResponse(html)
+        # html = '<div>Hello</div>'
+        return render(request, 'post.html')
         
