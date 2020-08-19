d358d79c5fd1a15fd386fdb77fffda9ac3242590
dtmkeng <phumai@odds.team>
2020-08-19 22:34:06 +0700

    add models gnder and relation

--- postit/students/admin.py
+++ postit/students/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Gender
 
 
 # Register your models here.
@@ -8,4 +8,11 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'age',
+        'gender',
+    )
+    
+@admin.register(Gender)
+class GenderAdmin(admin.ModelAdmin):
+    list_display = (
+        'gender',
     )
--- postit/students/models.py
+++ postit/students/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Gender(models.Model):
+    gender =  models.CharField(max_length=60)
+    
+    def __str__(self):
+        return self.gender
+
 class Student(models.Model):
     name = models.TextField(max_length=50)
     age = models.DecimalField(max_digits=5, decimal_places=0)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
+
