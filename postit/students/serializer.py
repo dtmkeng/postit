eed6e91a0d2066c6751bae488542e1d519b50c79
dtmkeng <phumai@odds.team>
2020-08-20 10:26:50 +0700

    serializer

--- postit/students/serializer.py
+++ postit/students/serializer.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 
+class GenderSerializer(serializers.Serializer):
+    gender = serializers.CharField()
+
 class StudentSerializer(serializers.Serializer):
     name = serializers.CharField()
     age = serializers.DecimalField(max_digits=5, decimal_places=0)
+    gender = GenderSerializer()
